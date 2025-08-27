92605fcc504656c3128ddad393a2050b10cbd12a
John Smith <smithjf1988@gmail.com>
2025-08-27 15:44:33 -0500

    adding Link functionality to footer, shows active page and are now clickable links

--- fotoTree/src/components/footer/Footer.jsx
+++ fotoTree/src/components/footer/Footer.jsx
@@ -1,29 +1,37 @@
-import './Footer.css'
+import { Link, useLocation } from "react-router-dom";
+import './Footer.css';
 
 const Footer = () => {
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <>
-    <hr />
-
-    <br />
-
-    <div className='container'>
-     <div>Home</div>
-     <div>My Tree</div>
-     <div>Friends</div>
-     <div>Photo Viewer</div>
-     <div>Settings</div>   
-    </div>
-
-    <br />
-
-    <div>
-        <p>
-            All rights reserved by FotoTree 2025 incorporated 
-        </p>
-    </div>
+      <hr />
+      <br />
+      <div className='footer-container'>
+        <Link to="/" className={`footer-link ${isActive("/") ? "active" : ""}`}>
+          Home
+        </Link>
+        <Link to="/tree" className={`footer-link ${isActive("/tree") ? "active" : ""}`}>
+          My Tree
+        </Link>
+        <Link to="/people" className={`footer-link ${isActive("/people") ? "active" : ""}`}>
+          My Family
+        </Link>
+        <Link to="/person" className={`footer-link ${isActive("/person") ? "active" : ""}`}>
+          Add Family Member
+        </Link>
+        <span className="footer-link disabled">About</span>
+        <span className="footer-link disabled">My Account</span>
+      </div>
+      <br />
+      <div className="footer-bottom">
+        <p>All rights reserved by FotoTree 2025 incorporated</p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
